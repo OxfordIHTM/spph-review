8ad63d7787ce258fb65c3d9c532dfb81c2420df0
Ernest Guevarra <ernest@guevarra.io>
2025-09-26 10:18:47 +0100

    create targets and functions for UCLA ph programmes; fix #26

--- _targets_data.R
+++ _targets_data.R
@@ -167,5 +167,21 @@ aspher_data_targets <- tar_plan(
 )
 
 
+## UCLA ----
+
+ucla_data_targets <- tar_plan(
+  ucla_ph_master_programme_base_link = "https://ph.ucla.edu/degrees-programs/find-compare-degree-programs?degree_program[]=198,202,199",
+  ucla_ph_master_programme_page = seq_len(3),
+  tar_target(
+    name = ucla_ph_master_programme_links,
+    command = ucla_get_master_programme_links(
+      base_url = ucla_ph_master_programme_base_link, 
+      page = ucla_ph_master_programme_page 
+    ),
+    pattern = map(ucla_ph_master_programme_page)
+  )
+)
+
+
 
 
