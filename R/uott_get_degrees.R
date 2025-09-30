7dc415d3ea668ee956e94646e65f9661d2369b5e
Ernest Guevarra <ernest@guevarra.io>
2025-09-30 23:14:01 +0100

    add institution to UOtt degree list; fix #128

--- R/uott_get_degrees.R
+++ R/uott_get_degrees.R
@@ -4,6 +4,7 @@
 
 uott_get_master_programme_links <- function() {
   tibble::tibble(
+    institution = "University of Ottawa School of Epidemiology and Public Health",
     department = NA_character_,
     degree = "Master of Public Health",
     url = "https://www.uottawa.ca/faculty-medicine/graduate-postdoctoral/programs-admission/program-information/MPH"
