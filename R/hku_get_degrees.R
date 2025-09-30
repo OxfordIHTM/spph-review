59d6e3efb6267c3be178e3221fa815f4b909385b
Ernest Guevarra <ernest@guevarra.io>
2025-09-30 22:14:16 +0100

    add institution to HKU degree list; fix #120

--- R/hku_get_degrees.R
+++ R/hku_get_degrees.R
@@ -4,6 +4,7 @@
 
 hku_get_master_programme_links <- function(.url) {
   tibble::tibble(
+    institution = "Hong Kong University School of Public Health",
     department = NA_character_,
     degree = "Master of Public Health",
     url = .url
