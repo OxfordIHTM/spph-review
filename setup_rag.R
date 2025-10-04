55f7ab43ce61d9811e1653525714589dc7d6d17f
Ernest Guevarra <ernest@guevarra.io>
2025-10-04 22:50:13 +0100

    setup RAG; fix #172; fix #173

--- setup_rag.R
+++ setup_rag.R
@@ -1,6 +1,11 @@
 # Setup RAG --------------------------------------------------------------------
 
 
+## Load libraries and custom functions ----
+suppressPackageStartupMessages(source("packages.R"))
+for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
+
+
 ## Load targets ----
 
 tar_load(ph_master_programme_links)
