d99ca7ec3862959001db3571c508bef2bd98316e
Ernest Guevarra <ernest@guevarra.io>
2025-10-05 15:41:29 +0100

    setup gha workflow for RAG

--- setup_rag_set2.R
+++ setup_rag_set2.R
@@ -3,24 +3,8 @@
 
 ##
 
-## Load libraries and custom functions ----
-suppressPackageStartupMessages(source("packages.R"))
-for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
-
-
-## Load targets ----
-
-tar_load(ph_master_programme_links)
-
-
-## Create store ----
-
-phdegree_store_location <- "phdegree.duckdb"
-
-##
-
 x <- grep(
-  pattern = "http://www.mphpublichealthpractice.uw.edu/",
+  pattern = "https://www.onlinemph.uw.edu/",
   x = ph_master_programme_links
 )
 
