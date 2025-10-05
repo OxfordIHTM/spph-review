ad3e52a1c78edcba8c49a35281084e10eae1d352
Ernest Guevarra <ernest@guevarra.io>
2025-10-05 15:41:29 +0100

    setup gha workflow for RAG

---
+++ setup_rag_set2.R
@@ -0,0 +1,11 @@
+
+x <- grep(
+  pattern = "http://www.mphpublichealthpractice.uw.edu/",
+  x = ph_master_programme_links
+)
+
+y <- ph_master_programme_links[x:length(ph_master_programme_links)]
+
+process_degrees(.url = y, store = phdegree_store)
+
+ragnar_store_build_index(phdegree_store)
