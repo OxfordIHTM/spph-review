c390a9f56863045bba763dc6584658907d6801b3
Ernest Guevarra <ernest@guevarra.io>
2025-10-05 15:41:29 +0100

    setup gha workflow for RAG

--- _targets_rag.R
+++ _targets_rag.R
@@ -4,6 +4,7 @@
 ## Setup data store for RAG ----
 
 ph_store_targets <- tar_plan(
+  db_gdrive_id = "1lfY9-WEuMO0te6twZSH2a0xqwzba6mkq"
 )
 
 
--- setup_rag.R
+++ setup_rag.R
@@ -9,6 +9,7 @@ for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 ## Load targets ----
 
 tar_load(ph_master_programme_links)
+tar_load(db_gdrive_id)
 
 
 ## Create store ----
@@ -27,3 +28,12 @@ phdegree_store <- ragnar::ragnar_store_create(
 process_degrees(.url = ph_master_programme_links, store = phdegree_store)
 
 ragnar_store_build_index(phdegree_store)
+
+
+## Send duckdb to Google Drive ----
+
+db_gdrive_upload(
+  db = "phdegree.duckdb",
+  name = "phdegree.duckdb",
+  gdrive_id = db_gdrive_id
+)
