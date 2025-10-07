1eb2d781cf0b0c91cde1b6ac7cbee7b4ed61cfec
Ernest Guevarra <ernest@guevarra.io>
2025-10-07 20:23:00 +0100

    return to grouped ingest

--- setup_rag_iteration.R
+++ setup_rag_iteration.R
@@ -1,15 +1,15 @@
 # Iterate on RAG ingest --------------------------------------------------------
 
 ## Load libraries and custom functions ----
-suppressPackageStartupMessages(source("packages.R"))
-for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
+# suppressPackageStartupMessages(source("packages.R"))
+# for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 
 ## Load targets ----
 
-tar_load(ph_master_programme_links)
+#tar_load(ph_master_programme_links)
 
-process_degrees(.url = ph_master_programme_links[81:333], store = phdegree_store)
+process_degrees(.url = ph_master_programme_links[147:333], store = phdegree_store)
 
 ### Index store ----
 ragnar_store_build_index(phdegree_store)
