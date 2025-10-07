cfb819714b2428b3dcfb85be280e83bb2e15d198
Ernest Guevarra <ernest@guevarra.io>
2025-10-07 12:25:43 +0100

    refactor release function

--- R/release_db.R
+++ R/release_db.R
@@ -10,9 +10,13 @@ release_rag_db <- function(file, name) {
     sep = "-"
   )
 
-  piggyback::pb_new_release(tag = tag, body = "Database release")
+  releases <- piggyback::pb_releases()
 
-  piggyback::pb_upload(file = file, name = name)
+  if (!grepl(pattern = tag, x = releases$release_name)) {
+    piggyback::pb_new_release(tag = tag, body = "Database release")
+  }
 
-  name
+  Sys.sleep(time = 60)
+
+  piggyback::pb_upload(file = file, name = name, tag = tag)
 }
