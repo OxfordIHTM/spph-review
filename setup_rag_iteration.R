0a9acba1f0ffdb0aaf99370393bea7f8be9cd605
Ernest Guevarra <ernest@guevarra.io>
2025-10-07 20:23:00 +0100

    return to grouped ingest

--- setup_rag_iteration.R
+++ setup_rag_iteration.R
@@ -1,6 +1,6 @@
 # Iterate on RAG ingest --------------------------------------------------------
 
-process_degrees(.url = ph_master_programme_links[], store = phdegree_store)
+process_degrees(.url = ph_master_programme_links[81:333], store = phdegree_store)
 
 ### Index store ----
 ragnar_store_build_index(phdegree_store)
