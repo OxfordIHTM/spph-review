1099d56c2c029c4fb94e2448879dcef6a696b95a
Ernest Guevarra <ernest@guevarra.io>
2025-10-07 04:14:47 +0100

    refactor rag ingest

--- setup_rag.R
+++ setup_rag.R
@@ -32,50 +32,90 @@ phdegree_store <- ragnar::ragnar_store_create(
 ## Ingest degree information ----
 
 ### Set 1 ----
-message("Reading information from degrees 1 to 50")
-process_degrees(.url = ph_master_programme_links[1:50], store = phdegree_store)
+message("Reading information from degrees 1 to 20")
+process_degrees(.url = ph_master_programme_links[1:20], store = phdegree_store)
 
 message("Pausing ingest for 30 seconds")
 Sys.sleep(30)
 
 ### Set 2 ----
-message("Reading information from degrees 51 to 100")
-process_degrees(.url = ph_master_programme_links[51:100], store = phdegree_store)
+message("Reading information from degrees 21 to 40")
+process_degrees(.url = ph_master_programme_links[21:40], store = phdegree_store)
 
 message("Pausing ingest for 30 seconds")
 Sys.sleep(30)
 
 ### Set 3 ----
-message("Reading information from degrees 101 to 150")
-process_degrees(.url = ph_master_programme_links[101:150], store = phdegree_store)
+message("Reading information from degrees 41 to 60")
+process_degrees(.url = ph_master_programme_links[41:60], store = phdegree_store)
 
 message("Pausing ingest for 30 seconds")
 Sys.sleep(30)
 
 ### Set 4 ----
-message("Reading information from degrees 151 to 200")
-process_degrees(.url = ph_master_programme_links[151:200], store = phdegree_store)
+message("Reading information from degrees 61 to 80")
+process_degrees(.url = ph_master_programme_links[61:80], store = phdegree_store)
 
 message("Pausing ingest for 30 seconds")
 Sys.sleep(30)
 
 ### Set 5 ----
-message("Reading information from degrees 201 to 250")
-process_degrees(.url = ph_master_programme_links[201:250], store = phdegree_store)
+message("Reading information from degrees 81 to 100")
+process_degrees(.url = ph_master_programme_links[81:100], store = phdegree_store)
 
 message("Pausing ingest for 30 seconds")
 Sys.sleep(30)
 
 ### Set 6 ----
-message("Reading information from degrees 251 to 300")
-process_degrees(.url = ph_master_programme_links[251:300], store = phdegree_store)
+message("Reading information from degrees 101 to 120")
+process_degrees(.url = ph_master_programme_links[101:120], store = phdegree_store)
 
 message("Pausing ingest for 30 seconds")
 Sys.sleep(30)
 
 ### Set 7 ----
-message("Reading information from degrees 301 to 333")
-process_degrees(.url = ph_master_programme_links[301:333], store = phdegree_store)
+message("Reading information from degrees 121 to 140")
+process_degrees(.url = ph_master_programme_links[121:140], store = phdegree_store)
+
+### Set 8 ----
+message("Reading information from degrees 141 to 160")
+process_degrees(.url = ph_master_programme_links[141:160], store = phdegree_store)
+
+### Set 9 ----
+message("Reading information from degrees 161 to 180")
+process_degrees(.url = ph_master_programme_links[161:180], store = phdegree_store)
+
+### Set 10 ----
+message("Reading information from degrees 181 to 200")
+process_degrees(.url = ph_master_programme_links[181:200], store = phdegree_store)
+
+### Set 11 ----
+message("Reading information from degrees 201 to 220")
+process_degrees(.url = ph_master_programme_links[201:220], store = phdegree_store)
+
+### Set 12 ----
+message("Reading information from degrees 221 to 240")
+process_degrees(.url = ph_master_programme_links[221:240], store = phdegree_store)
+
+### Set 13 ----
+message("Reading information from degrees 241 to 260")
+process_degrees(.url = ph_master_programme_links[241:260], store = phdegree_store)
+
+### Set 14 ----
+message("Reading information from degrees 261 to 280")
+process_degrees(.url = ph_master_programme_links[261:280], store = phdegree_store)
+
+### Set 15 ----
+message("Reading information from degrees 281 to 300")
+process_degrees(.url = ph_master_programme_links[281:300], store = phdegree_store)
+
+### Set 16 ----
+message("Reading information from degrees 301 to 320")
+process_degrees(.url = ph_master_programme_links[301:320], store = phdegree_store)
+
+### Set 17 ----
+message("Reading information from degrees 321 to 333")
+process_degrees(.url = ph_master_programme_links[321:333], store = phdegree_store)
 
 ### Index store ----
 ragnar_store_build_index(phdegree_store)
