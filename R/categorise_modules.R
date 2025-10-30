50c8546a08cd80c4a378e7796bf9efc6bda4535c
Ernest Guevarra <ernest@guevarra.io>
2025-10-30 04:44:13 +0000

    extract categories

--- R/categorise_modules.R
+++ R/categorise_modules.R
@@ -38,27 +38,6 @@ create_module_categories <- function(modules, model) {
 #' Categorise modules
 #' 
 
-# categorise_module <- function(module, module_categories, model) {
-#   system_prompt = paste0(
-#     "You are a terse assistant. Classify ", module, 
-#     " into one of the following categories: ", 
-#     paste(module_categories, collapse = ", "), "."
-#   )
-
-#   chat <- ellmer::chat_ollama(system_prompt = system_prompt,  model = model)
-
-#   df <- chat$chat_structured(
-#     module, 
-#     type = ellmer::type_enum(
-#       values = module_categories,
-#       description = "Category to assign to the given text"
-#     )
-#   )
-
-#   df
-# }
-
-
 categorise_module <- function(module, module_categories, model) {
   system_prompt = paste0(
     "You are a terse assistant. Classify ", module, 
@@ -68,7 +47,29 @@ categorise_module <- function(module, module_categories, model) {
 
   chat <- ellmer::chat_ollama(model = model)
 
-  chat$chat(system_prompt)
+  type_category <- ellmer::type_object(
+    category = ellmer::type_enum(
+      values = module_categories,
+      description = "Category to assign to the given text"
+    )
+  )
+
+  df <- chat$chat_structured(module, type = type_category)
+
+  df
 }
+
+
+# categorise_module <- function(module, module_categories, model) {
+#   system_prompt = paste0(
+#     "You are a terse assistant. Classify ", module, 
+#     " into one of the following categories: ", 
+#     paste(module_categories, collapse = ", "), "."
+#   )
+
+#   chat <- ellmer::chat_ollama(model = model)
+
+#   chat$chat(system_prompt)
+# }
 
 
