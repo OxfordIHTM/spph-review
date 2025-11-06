772a9088253a290f6252a6b14247995fe2707326
Ernest Guevarra <ernest@guevarra.io>
2025-11-06 11:46:54 +0000

    refactor release outputs functions

--- R/release_outputs.R
+++ R/release_outputs.R
@@ -8,82 +8,94 @@ release_rag_db <- function(file,
                            type = c("major", "minor", "patch")) {
   type <- match.arg(type)
 
-  if (!is.null(tag)) {
-    prev_release_tag <- piggyback::pb_releases() |>
-      dplyr::slice(1) |>
-      dplyr::pull(tag_name) |>
-      stringr::str_split(pattern = "\\.") |>
-      unlist()
-
-    tag <- prev_release_tag
-
-    if (type == "major") {
-      tag[1] <- tag[1] |>
-        stringr::str_replace(
-          pattern = "[0-9]", 
-          replacement = stringr::str_extract(
-            string = tag[1], pattern = "[0-9]"
-          ) |>
-            as.numeric() |>
-            sum(1) |>
-            as.character()
-        )
-      
-      tag[2] <- 0
-      tag[3] <- 0
-
-      tag <- paste(tag, collapse = ".")
-    }
-
-    if (type == "minor") {
-      tag[2] <- tag[2] |>
-        stringr::str_replace(
-          pattern = "[0-9]", 
-          replacement = stringr::str_extract(
-            string = tag[2], pattern = "[0-9]"
-          ) |>
-            as.numeric() |>
-            sum(1) |>
-            as.character()
-        )
-      
-      tag[3] <- 0
-
-      tag <- paste(tag, collapse = ".")
-    }
-
-    if (type == "patch") {
-      tag[3] <- tag[3] |>
-        stringr::str_replace(
-          pattern = "[0-9]", 
-          replacement = stringr::str_extract(
-            string = tag[3], pattern = "[0-9]"
-          ) |>
-            as.numeric() |>
-            sum(1) |>
-            as.character()
-        )
-      
-      tag <- paste(tag, collapse = ".")
-    }
-  }
-  
   releases <- piggyback::pb_releases()
 
-  if (ncol(releases) == 0) {
-    piggyback::pb_new_release(
-      tag = tag,
-      name = paste(name, tag),
-      body = "Database release"
-    )
-  }
-  
-  if (!tag %in% releases$tag_name) {
-    piggyback::pb_new_release(
-      tag = tag,
-      name = paste(name, tag),
-      body = "Database release"
-    )
+  if (is.null(tag)) {
+    if (ncol(releases) == 0) {
+      if (type == "major") {
+        tag <- "v1.0.0"
+      }
+
+      if (type == "minor") {
+        tag <- "v0.1.0"
+      }
+
+      if (type == "path") {
+        tag <- "v0.0.1"
+      }
+
+      piggyback::pb_new_release(
+        tag = tag,
+        name = paste(name, tag),
+        body = "Database release"
+      )
+    } else {
+      prev_release_tag <- piggyback::pb_releases() |>
+        dplyr::slice(1) |>
+        dplyr::pull(tag_name) |>
+        stringr::str_split(pattern = "\\.") |>
+        unlist()
+
+      tag <- prev_release_tag
+
+      if (type == "major") {
+        tag[1] <- tag[1] |>
+          stringr::str_replace(
+            pattern = "[0-9]", 
+            replacement = stringr::str_extract(
+              string = tag[1], pattern = "[0-9]"
+            ) |>
+              as.numeric() |>
+              sum(1) |>
+              as.character()
+          )
+      
+        tag[2] <- 0
+        tag[3] <- 0
+
+        tag <- paste(tag, collapse = ".")
+      }
+
+      if (type == "minor") {
+        tag[2] <- tag[2] |>
+          stringr::str_replace(
+            pattern = "[0-9]", 
+            replacement = stringr::str_extract(
+              string = tag[2], pattern = "[0-9]"
+            ) |>
+              as.numeric() |>
+              sum(1) |>
+              as.character()
+          )
+      
+        tag[3] <- 0
+
+        tag <- paste(tag, collapse = ".")
+      }
+
+      if (type == "patch") {
+        tag[3] <- tag[3] |>
+          stringr::str_replace(
+            pattern = "[0-9]", 
+            replacement = stringr::str_extract(
+              string = tag[3], pattern = "[0-9]"
+            ) |>
+              as.numeric() |>
+              sum(1) |>
+              as.character()
+          )
+      
+        tag <- paste(tag, collapse = ".")
+      }
+    }
+  } else {
+    if (!tag %in% releases$tag_name) {
+      piggyback::pb_new_release(
+        tag = tag,
+        name = paste(name, tag),
+        body = "Database release"
+      )
+    }
   }
 
   Sys.sleep(time = 60)
